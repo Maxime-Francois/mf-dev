a32433dabeef2e668569241123165f660e1c9ada
Maxime-Francois <maximefrancois92@gmail.com>
2021-06-03 16:32:23 +0200

    integration profil et portfolio

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ $(document).ready(function(){
              
 
         }
-    })
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){  
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+
+    });
+
 });
